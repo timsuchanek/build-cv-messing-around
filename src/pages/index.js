f5fd62ebafb5e07d97978d75eb0435b991f22e8a
Andre <itstheandre@gmail.com>
2020-08-10 02:15:45 +0200

    cleaned up example. single page with minimal imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { prisma } from "../utils/prismaClient";
+// import { startingForm, prisma } from "../utils"; // Like this it will cause the error: Can't resolve 'async_hooks' in @prisma/cluent/runtime
+// import { startingForm } from "../utils"; // Like this it will cause the error: Can't resolve 'async_hooks' in @prisma/cluent/runtime
 import { startingForm } from "../utils/startingForm";
-// import { startingForm, getAll } from "../utils";
+// import { prisma } from "../utils"; // Here prisma gets imported nicely as well. Its only whenever I try to import startingForm from the same place
 
 const Index = (props) => {
   return <h1>Hello</h1>;
